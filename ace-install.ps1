7f268816cd77214cd037647ae4b1bfd8ab32ca44
Abhinab Sarkar <abhinab.sarkar@gmail.com>
2020-04-13 01:11:01 +0000

    Update ace-install.ps1

--- ace-install.ps1
+++ ace-install.ps1
@@ -1,3 +1,5 @@
+New-Item -Path 'D:\temp' -ItemType Directory
+<#
 # Install wsl feature
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -Verbose
 # Install Azure CLI. Since using the Invoke-WebRequest, must specify the parameter -UseBasicParsing else it will fail
@@ -6,3 +8,4 @@ Invoke-WebRequest -UseBasicParsing -Uri https://aka.ms/installazurecliwindows -O
 # Install kubectl 
 Install-Script -Name install-kubectl -Scope CurrentUser -Force -Verbose
 install-kubectl.ps1
+#>
