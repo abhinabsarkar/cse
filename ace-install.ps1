1ed202d0ffd6fd5cdffdce3bb3482612a96965cd
Abhinab Sarkar <abhinab.sarkar@gmail.com>
2020-05-09 17:22:36 +0000

    Update ace-install.ps1

--- ace-install.ps1
+++ ace-install.ps1
@@ -1,12 +1,11 @@
 # Creating folder 'temp' in D: for testing
-New-Item -Path 'D:\temp' -ItemType Directory
-<#
-# Install wsl feature
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -Verbose
-# Install Azure CLI. Since using the Invoke-WebRequest, must specify the parameter -UseBasicParsing else it will fail
+New-Item -Path 'c:\abhinab\kubectl' -ItemType Directory
+# Install Azure CLI for windows. Since using the Invoke-WebRequest, must specify the parameter -UseBasicParsing else it will fail
 Invoke-WebRequest -UseBasicParsing -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; `
     Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi -Verbose
-# Install kubectl 
-Install-Script -Name install-kubectl -Scope CurrentUser -Force -Verbose
-install-kubectl.ps1
-#>
+# Install wsl feature
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart -OutVariable results -Verbose
+if ($results.RestartNeeded -eq $true) {
+  Restart-Computer -Force
+}
+# Install kubectl for windows
